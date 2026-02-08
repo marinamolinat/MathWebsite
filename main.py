d1e784ad2de83bb507ca975c748e2b99f12b335b
Marina M <marinamolina7877@gmail.com>
2026-02-08 11:25:16 -0500

    started playing around with flask lol, i have to learn how to use it

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    myname = request.args.get('name')
+    if myname is None:
+        myname = "Guest"
+    
+    return render_template('login.html', name=myname)
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000, debug=True)
